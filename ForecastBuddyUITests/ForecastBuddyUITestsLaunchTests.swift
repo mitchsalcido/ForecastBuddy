76bec9ec85e7551b581e263fd7a72460ed0ead6b
Mitch Salcido <salcidoconsulting@mac.com>
2022-07-29 07:45:55 -0700

    Initial Commit

---
+++ ForecastBuddyUITests/ForecastBuddyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ForecastBuddyUITestsLaunchTests.swift
+//  ForecastBuddyUITests
+//
+//  Created by Mitchell Salcido on 7/29/22.
+//
+
+import XCTest
+
+class ForecastBuddyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
