00490ebb83af8619e88d394e74760b976d0af7f3
Mitch Salcido <salcidoconsulting@mac.com>
2022-08-20 14:28:37 -0700

    functional

--- ForecastBuddy/Controller/ForecastTableViewController.swift
+++ ForecastBuddy/Controller/ForecastTableViewController.swift
@@ -12,7 +12,6 @@ class ForecastTableViewController: UITableViewController, NSFetchedResultsContro
     
     var forecast:Forecast!
     var degreesF:Bool!
-    var dailyForecastArray:[[String:[HourlyWeather]]] = []
     var dataController:CoreDataController!
     var fetchedResultsController:NSFetchedResultsController<HourlyForecast>!
     
@@ -78,18 +77,6 @@ extension ForecastTableViewController {
 
 extension ForecastTableViewController {
     
-    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        print("didChangeContent")
-    }
-    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        print("willChangeContent")
-    }
-    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
-        print("didChangeAtForNew")
-    }
-}
-extension ForecastTableViewController {
-    
     func configureHourlyForecastFrc() {
         
         let request:NSFetchRequest<HourlyForecast> = NSFetchRequest(entityName: "HourlyForecast")
@@ -100,59 +87,13 @@ extension ForecastTableViewController {
         request.predicate = predicate
         
         fetchedResultsController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: dataController.viewContext, sectionNameKeyPath: "dayOfWeek", cacheName: nil)
-        
-        fetchedResultsController.delegate = self
-        
+
         do {
             try fetchedResultsController.performFetch()
             tableView.reloadData()
         } catch {
             // TODO: alert error
         }
-    }
-    
-    func configureDailyForecastArray() -> [[String:[HourlyWeather]]] {
-        
-        var result:[[String:[HourlyWeather]]] = [] // [[Day:[HourlyWeather]]]
-        
-        guard let hourlyForecasts = fetchedResultsController.fetchedObjects, !hourlyForecasts.isEmpty else {
-            return result
-        }
-        
-        var days:[String] = []
-        for hourlyForecast in hourlyForecasts {
-            if let dayOfWeek = hourlyForecast.date?.dayString() {
-                if !days.contains(dayOfWeek) {
-                    days.append(dayOfWeek)
-                }
-            }
-        }
-        
-        var hourlyResponses:[[HourlyWeather]] = []
-        for day in days {
-            var hourly:[HourlyWeather] = []
-            for hourlyForecast in hourlyForecasts {
-                
-                if let date = hourlyForecast.date, let icon = hourlyForecast.name, let description = hourlyForecast.weatherDescription {
-                    
-                    let hourlyWeather = HourlyWeather(icon: icon, date: date, temperature: hourlyForecast.temperatureKelvin, description: description)
-                    
-                    if day == date.dayString() {
-                        hourly.append(hourlyWeather)
-                    }
-                }
-            }
-            hourlyResponses.append(hourly)
-        }
-        
-        days.remove(at: 0)
-        days.insert("Today", at: 0)
-        
-        for (index, day) in days.enumerated() {
-            result.append([day:hourlyResponses[index]])
-        }
-        
-        return result
     }
 }
 
